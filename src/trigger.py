393518091cc15714042fb5e7b3ef8b2f6a3a9347
Hipass7 <qjsro8763@daum.net>
2024-07-28 18:30:47 +0900

    [230728] add trigger for down direction (SIGUSR2)

--- src/trigger.py
+++ src/trigger.py
@@ -5,6 +5,10 @@ import sys
 
 if __name__ == "__main__":
     pid = sys.argv[1]
+    direction = sys.argv[2]
 
     # 자식 프로세스에 사용자 정의 신호 보내기
-    os.kill(int(pid), signal.SIGUSR1)
+    if int(direction) == 1:
+        os.kill(int(pid), signal.SIGUSR1)
+    else:
+        os.kill(int(pid), signal.SIGUSR2)
